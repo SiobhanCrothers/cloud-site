1cc529a68331fda7ed8b0ab5210245275ead41cb
Siobhan Crothers <siobhan.crothers@bravurasecurity.com>
2023-09-12 15:45:56 -0600

    Commiting work with search bar working

---
+++ gatsby-starter-blog-with-lunr/src/components/searchResults.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import "../styles/with-lunr.css"; 
+
+const SearchResults = ({ results, query }) => ( 
+<div>
+{!!results.length && query && 
+  <h2
+    className="search-results-count"
+    id="search-results-count"
+    aria-live="assertive"
+  >Found {results.length} posts on "{query}"</h2>
+}
+{!!results.length &&
+  <ol className="search-results-list">
+    {results.map(({
+      title,
+      url,
+      date,
+      description
+    }
+) => (
+      <li key={title}>
+        <h3 className="search-results-list__heading">
+          <a href={url} className="search-results-list__link">
+            {title}
+          </a>
+        </h3>
+        <small>{(new Date(date).toLocaleString('en-GB'))}</small>
+        {description && <p>{description}</p>}
+      </li>
+    ))}
+  </ol>
+}
+</div>
+);
+
+export default SearchResults;
