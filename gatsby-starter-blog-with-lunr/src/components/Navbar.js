6d5a654092e7df29390a68a5d89fdce8e4755dce
Siobhan Crothers <siobhan.crothers@bravurasecurity.com>
2023-09-18 14:39:56 -0600

    Removing sample forms

--- gatsby-starter-blog-with-lunr/src/components/Navbar.js
+++ gatsby-starter-blog-with-lunr/src/components/Navbar.js
@@ -64,11 +64,11 @@ const Navbar = () => {
               Contact
             </Link>
             </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
+	  {/*<li className="navbar-item" style={{padding: "0px"}}>
             <Link className="navbar-item" to="/contact/examples">
               Form Examples
             </Link>
-            </li>
+            </li>*/}
 	  {/*<li className="navbar-item" style={{padding: "0px"}}>
             <Link className="navbar-item" to="/faq">
               FAQ
