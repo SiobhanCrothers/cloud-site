0122e81a460b2bb9284a0a2aca2bc50fbfdb20a7
Siobhan Crothers <siobhan.crothers@bravurasecurity.com>
2023-09-19 08:57:51 -0600

    Fixing image margins on welcome icons

--- gatsby-starter-blog-with-lunr/src/components/WelcomeRoll.js
+++ gatsby-starter-blog-with-lunr/src/components/WelcomeRoll.js
@@ -14,7 +14,7 @@ const WelcomeRollTemplate = (props) => {
         posts.map(({ node: post }) => (
           <div className="is-parent column is-4" key={post.id} >
 	    <article 
-              className={`blog-list-item tile is-child box notification ${
+              className={`welcome-list-item tile is-child box notification ${
                 post.frontmatter.featuredpost ? 'is-featured' : ''
               }` }
             >
