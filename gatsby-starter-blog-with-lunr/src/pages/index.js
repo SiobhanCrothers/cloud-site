a6162e9ec4e42b7db8bf6796e3cc95a67a6dd747
Siobhan Crothers <siobhan.crothers@bravurasecurity.com>
2023-09-20 12:01:42 -0600

    Removing unused index files for landing page. Duplicated welcome page index.js file so that the landing page would look the same.

---
+++ gatsby-starter-blog-with-lunr/src/pages/index.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+import Layout from "../components/Layout";
+import WelcomeRoll from "../components/WelcomeRoll";
+
+export default class WelcomeIndexPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <h1
+                className="has-text-weight-bold is-size-1"
+                style={{
+                  boxShadow: "0 0 0 #fff, -0.5rem 0 0 #f65e0a",
+                  backgroundColor: "#fff",
+                  color: "#f65e0a",
+                  padding: "1rem",
+                }}
+              >
+                Welcome to Bravura Cloud
+              </h1>
+              <WelcomeRoll />
+            </div>
+          </div>
+        </section>
+      </Layout>
+    );
+  }
+}
