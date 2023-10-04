721b19dc2f730382f8f55217569b6eb57445e287
Siobhan Crothers <siobhan.crothers@bravurasecurity.com>
2023-10-04 01:05:41 -0600

    Fixing search so it allows user to complete typing before navigating to the results page

--- gatsby-starter-blog-with-lunr/src/components/searchForm.js
+++ gatsby-starter-blog-with-lunr/src/components/searchForm.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { navigate } from 'gatsby';
 
-const SearchForm = ({ query }) => (
-  <form role="search" method="GET">
-    <label htmlFor="search-input"><h1>Search posts</h1></label>
-    <input
-      type="search"
-      id="search-input"
-      name="keywords"
-      aria-controls="search-results-count"
-      onChange={(e) => navigate(`/search?keywords=${encodeURIComponent(e.target.value)}`)}
-      value={query}
-    />
-    <button type="submit">Submit</button>
-  </form>
-);
+const SearchForm = () => {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();  // Prevent the default form submission behavior
+    navigate(`/search?keywords=${encodeURIComponent(query)}`);
+  };
+
+  return (
+    <form role="search" onSubmit={handleSubmit}>
+      <label htmlFor="search-input"><h1>Search posts</h1></label>
+      <input
+        type="search"
+        id="search-input"
+        name="keywords"
+        aria-controls="search-results-count"
+        onChange={handleInputChange}
+        value={query}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default SearchForm;
+
