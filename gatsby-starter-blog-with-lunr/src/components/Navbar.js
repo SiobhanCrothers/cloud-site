e932b39eebfa87173676d1ddccb96ed202700893
Siobhan Crothers <siobhan.crothers@bravurasecurity.com>
2023-10-06 09:35:35 -0600

    Adding Documentation menu items

--- gatsby-starter-blog-with-lunr/src/components/Navbar.js
+++ gatsby-starter-blog-with-lunr/src/components/Navbar.js
@@ -3,6 +3,45 @@ import { Link } from "gatsby";
 import SearchForm from './searchForm';
 import github from "../img/github-icon.svg";
 import logo from "../img/logo.svg";
+import styled from 'styled-components';
+
+/* Style for the dropdown menu */
+
+const TopDropdownMenu = styled.ul`
+  display: none;
+  position: absolute;
+  background-color: #fff; /* Background color for the dropdown */
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);}
+`;
+
+const TopDropdownWrapper = styled.div`
+  position: relative;
+
+  &:hover ${TopDropdownMenu} {
+    display: block;
+  }
+
+  button {
+    cursor: pointer;
+    background: none;
+    border: none;
+    font: inherit;
+    color: inherit;
+    display: flex;
+    align-items: center;
+  }
+
+
+  &:hover button {
+    color: hsl(229, 53%, 53%);
+  }
+
+  .chevron {
+    margin-left: 0.5rem;
+    transform: rotate(90deg);
+  }
+`;
+
 
 const Navbar = () => {
   const [isActive, setIsActive] = useState(false);
@@ -30,50 +69,49 @@ const Navbar = () => {
           </button>
         </div>
         <ul id="navMenu" className={` navbar-start has-text-centered navbar-menu ${isActive && "is-active"}`}>
-            {/* TODO: inline override of padding is a result of refactoring
-                to a ul for accessibilty purposes, would like to see a css
-                re-write that makes this unneccesary.
-             */}
             <li className="navbar-item" style={{padding: "0px"}}>
               <Link className="navbar-item" to="/about">
                 About
               </Link>
             </li>
-	  {/*<li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/products">
-              Products
-            </Link>
-	    </li>*/}
             <li className="navbar-item" style={{padding: "0px"}}>
             <Link className="navbar-item" to="/configuration/quick-start">
               Quick Start
             </Link>
             </li>
             <li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/documentation">
-              Documentation
-            </Link>
+	      <TopDropdownWrapper>
+	        <button>
+	          Documentation 
+	  	  <span className="chevron">&gt;</span>
+	        </button>
+	        <TopDropdownMenu>
+	  	  <li className="navbar-item" style={{padding: "0px"}}>
+                    <Link to="/architecture">Architecture</Link>
+	  	  </li>
+ 	       {/*<li className="navbar-item" style={{padding: "0px"}}>
+                    <Link to="/configuration">Configuration</Link>
+                  </li>
+	  	  <li className="navbar-item" style={{padding: "0px"}}>
+                    <Link to="/features">Features</Link>
+                  </li>*/}
+                  <li className="navbar-item" style={{padding: "0px"}}>
+                    <Link to="/features/troubleshooting">Troubleshooting</Link>
+	          </li>
+                  <li className="navbar-item" style={{padding: "0px"}}>
+	            <Link to="/tech-concepts">Technical Concepts</Link>
+	          </li>
+	   	  <li className="navbar-item" style={{padding: "0px"}}>
+	            <Link to="/faq">FAQ</Link>
+	          </li>
+	  	</TopDropdownMenu>
+	      </TopDropdownWrapper>
             </li>
-	  {/*<li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/blog">
-              Blog
-            </Link>
-            </li>*/}
             <li className="navbar-item" style={{padding: "0px"}}>
             <Link className="navbar-item" to="/contact">
               Contact
             </Link>
             </li>
-	  {/*<li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/contact/examples">
-              Form Examples
-            </Link>
-            </li>*/}
-	  {/*<li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/faq">
-              FAQ
-            </Link>
-            </li>*/}
           <li className="navbar-item" style={{padding: "0px"}}>
             <a
               className="navbar-item"
